20716ace350c7766625ff5a4fac65a0e4d3d97bb
Milind <milindbadsar@gmail.com>
2025-04-25 02:30:08 +0530

    removed the code causing error

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,19 +1,3 @@
-// import { db, questionCollection } from "@/models/name";
-// import { databases } from "@/models/server/config";
-// import React from "react";
-// import EditQues from "./EditQues";
-
-// export default async function Page({
-//   params,
-// }: {
-//   params: { quesId: string; quesName: string };
-//   searchParams?: { [key: string]: string | string[] | undefined };
-// }) {
-//   const question = await databases.getDocument(
-//     db,
-//     questionCollection,
-//     params.quesId
-//   );
-
-//   return <EditQues question={question} />;
-// }
+export default async function Page() {
+  return null;
+}
