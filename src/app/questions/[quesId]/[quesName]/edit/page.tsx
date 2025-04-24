295511259e999866b3e36f6a53f6f94ea8a781b1
Milind <milindbadsar@gmail.com>
2025-04-25 02:19:34 +0530

    commented whole file causing error

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,19 +1,19 @@
-import { db, questionCollection } from "@/models/name";
-import { databases } from "@/models/server/config";
-import React from "react";
-import EditQues from "./EditQues";
+// import { db, questionCollection } from "@/models/name";
+// import { databases } from "@/models/server/config";
+// import React from "react";
+// import EditQues from "./EditQues";
 
-export default async function Page({
-  params,
-}: {
-  params: { quesId: string; quesName: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-  const question = await databases.getDocument(
-    db,
-    questionCollection,
-    params.quesId
-  );
+// export default async function Page({
+//   params,
+// }: {
+//   params: { quesId: string; quesName: string };
+//   searchParams?: { [key: string]: string | string[] | undefined };
+// }) {
+//   const question = await databases.getDocument(
+//     db,
+//     questionCollection,
+//     params.quesId
+//   );
 
-  return <EditQues question={question} />;
-}
+//   return <EditQues question={question} />;
+// }
