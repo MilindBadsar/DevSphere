05c3fccf9567b80cb0a98db6e198000aa8de8608
Milind <milindbadsar@gmail.com>
2025-04-26 09:21:55 +0530

    1 more change

--- src/app/users/[userId]/[userSlug]/votes/page.tsx
+++ src/app/users/[userId]/[userSlug]/votes/page.tsx
@@ -12,24 +12,23 @@ import Link from "next/link";
 import { Query } from "node-appwrite";
 import React from "react";
 
-const Page = async ({
-  params,
-  searchParams,
-}: {
-  params: { userId: string; userSlug: string };
-  searchParams: { page?: string; voteStatus?: "upvoted" | "downvoted" };
+const Page = async ({}: // params,
+// searchParams,
+{
+  // params: { userId: string; userSlug: string };
+  // searchParams: { page?: string; voteStatus?: "upvoted" | "downvoted" };
 }) => {
-  searchParams.page ||= "1";
+  // searchParams.page ||= "1";
 
   const query = [
-    Query.equal("votedById", params.userId),
+    Query.equal("votedById", "params.userId"),
     Query.orderDesc("$createdAt"),
-    Query.offset((+searchParams.page - 1) * 25),
+    // Query.offset((+searchParams.page - 1) * 25),
     Query.limit(25),
   ];
 
-  if (searchParams.voteStatus)
-    query.push(Query.equal("voteStatus", searchParams.voteStatus));
+  // if (searchParams.voteStatus)
+  //   query.push(Query.equal("voteStatus", searchParams.voteStatus));
 
   const votes = await databases.listDocuments(db, voteCollection, query);
 
@@ -75,34 +74,40 @@ const Page = async ({
         <ul className="flex gap-1">
           <li>
             <Link
-              href={`/users/${params.userId}/${params.userSlug}/votes`}
-              className={`block w-full rounded-full px-3 py-0.5 duration-200 ${
-                !searchParams.voteStatus ? "bg-white/20" : "hover:bg-white/20"
-              }`}
+              // href={`/users/${params.userId}/${params.userSlug}/votes`}
+              href={`/users/votes`}
+              // className={`block w-full rounded-full px-3 py-0.5 duration-200 ${
+              //   !searchParams.voteStatus ? "bg-white/20" : "hover:bg-white/20"
+              // }`}
+              className={`block w-full rounded-full px-3 py-0.5 duration-200`}
             >
               All
             </Link>
           </li>
           <li>
             <Link
-              href={`/users/${params.userId}/${params.userSlug}/votes?voteStatus=upvoted`}
-              className={`block w-full rounded-full px-3 py-0.5 duration-200 ${
-                searchParams?.voteStatus === "upvoted"
-                  ? "bg-white/20"
-                  : "hover:bg-white/20"
-              }`}
+              // href={`/users/${params.userId}/${params.userSlug}/votes?voteStatus=upvoted`}
+              // className={`block w-full rounded-full px-3 py-0.5 duration-200 ${
+              //   searchParams?.voteStatus === "upvoted"
+              //     ? "bg-white/20"
+              //     : "hover:bg-white/20"
+              // }`}
+              href={`/users/votes?voteStatus=upvoted`}
+              className={`block w-full rounded-full px-3 py-0.5 duration-200`}
             >
               Upvotes
             </Link>
           </li>
           <li>
             <Link
-              href={`/users/${params.userId}/${params.userSlug}/votes?voteStatus=downvoted`}
-              className={`block w-full rounded-full px-3 py-0.5 duration-200 ${
-                searchParams?.voteStatus === "downvoted"
-                  ? "bg-white/20"
-                  : "hover:bg-white/20"
-              }`}
+              // href={`/users/${params.userId}/${params.userSlug}/votes?voteStatus=downvoted`}
+              // className={`block w-full rounded-full px-3 py-0.5 duration-200 ${
+              //   searchParams?.voteStatus === "downvoted"
+              //     ? "bg-white/20"
+              //     : "hover:bg-white/20"
+              // }`}
+              href={`/users/votes?voteStatus=downvoted`}
+              className={`block w-full rounded-full px-3 py-0.5 duration-200`}
             >
               Downvotes
             </Link>
